1d058929427e3fd23395a583076989293a578c5b
gopinath <gopinathvcareer@gmail.com>
2024-03-02 11:18:25 +0530

    Initial commit

---
+++ calc/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    content = {'name': "Deena"}
+    return render(request, 'home.html', content)
+
+
+def result(request):
+    res_num = 0
+    num1 = int(request.POST['num1'])
+    num2 = int(request.POST['num2'])
+    res_num = num1 + num2
+    operation = request.POST['operation']
+
+    if operation =='add':
+        res_num = num1+num2
+    elif operation == 'sub':
+        res_num = num1 - num2
+    elif operation == 'div':
+        res_num = num1/num2
+    else:
+        res_num = num1 * num2
+
+    return render(request, 'result.html', {'result': res_num})
