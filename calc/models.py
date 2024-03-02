458ee14894bd501c2d72ad41fab0be5377d695f5
gopinath <gopinathvcareer@gmail.com>
2024-03-02 11:57:31 +0530

    Added models

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import timezone
 
-# Create your models here.
+class userProp(models.Model):
+    name = models.CharField(max_Length=100)
+    age = models.IntegerField()
+    is_allowed = models.BooleanField(default=True)
+    signed_at = models.DateTimeField(default=timezone.now())
+
